0c0ef2a9a8dc095a13ea052b2f142f28470e4c2e
JesseBarron <jessebarron1113@gmail.com>
2017-01-07 15:57:22 -0600

    Added functionality to the text input field and coded a keyup event

--- wikisearch.js
+++ wikisearch.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
 
+$("#text_input").keyup(function(e){
+    var key = e.which;
+    if(key == 13){
+      $("input[name=wikisearch]").click();
+      return false;
+  }
+});
+//This is the keyup event that triggers the click event below.(More in notes)
+
+
+$("input[name=Search]").click(function(){
+  var input = $("#text_input").val()
+
+//Api related stuff goes up here^^^^ hopefully....
+  console.log(input);
+  return false;
+  });
 });
